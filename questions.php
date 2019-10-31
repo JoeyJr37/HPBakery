79e3a923a05b71913696159480a4de2c68572e8b
Joey <55552636+JoeyJr37@users.noreply.github.com>
2019-10-31 10:00:49 -0500

    Updated AskAQuestion page with form

---
+++ questions.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+if($_POST["submit"]) {
+    $recipient="joeyjr11@gmail.com";
+    $subject="New Question via JustDesserts Website";
+    $sender=$_POST["firstName" + "_" + "lastName"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["question"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
+
+?>
